ebc80dae6e746fe54bb8b29f2b9995072d5cac1e
Jeremy Marin <marin.jeremy@gmail.com>
2019-11-24 00:34:28 +0900

    Revolve planets around the sun

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,39 @@
+import Planet from './planet.js'
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 100);
-
-controls = new THREE.OrbitControls( camera, renderer.domElement );
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.set(0, -2000, 500);
+new THREE.OrbitControls( camera, renderer.domElement );
 
 const scene = new THREE.Scene();
-
 const textureLoader = new THREE.TextureLoader();
 
-const sunGeometry = new THREE.SphereGeometry(10, 20, 20);
+const sunGeometry = new THREE.SphereGeometry(100, 20, 20);
 const sunTexture = textureLoader.load("textures/sun.jpg");
 const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
 const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+sun.rotation.x = 90;
 scene.add(sun);
 
-const sunlight = new THREE.PointLight(0xffffff, 2, 100);
+const sunlight = new THREE.PointLight(0xffffff, 2, 10000);
 sunlight.position.set(0, 0, 0);
 scene.add(sunlight);
 
-const planetGeometry = new THREE.SphereGeometry(10, 20, 20);
-const earthTexture = textureLoader.load("textures/earth.jpg");
-const planetMaterial = new THREE.MeshLambertMaterial({ map: earthTexture });
-const planet = new THREE.Mesh(planetGeometry, planetMaterial);
-planet.position.set(30, 0, 0)
-scene.add(planet);
+let planets = [];
+for (let i = 0; i < 10; i++) {
+  const planet = new Planet();
+  console.log(planet);
+  scene.add(planet.mesh);
+  planets.push(planet);
+}
 
 const update = () => {
+  planets.forEach(planet => {
+    planet.revolve();
+  })
 };
 
 const render = () => {
---
+++ src/js/planet.js
@@ -0,0 +1,27 @@
+export default class Planet {
+  constructor() {
+    this.color = new THREE.Color(Math.random(), Math.random(), Math.random());
+    this.radius = Math.random() * 100;
+    this.distance = 150 + Math.random() * 2000;
+    this.angle = Math.random() * 6;
+    this.revolutionSpeed = Math.random() / 100;
+
+    const planetGeometry = new THREE.SphereGeometry(this.radius, 20, 20);
+    const planetMaterial = new THREE.MeshLambertMaterial({ color: this.color });
+    this.mesh = new THREE.Mesh(planetGeometry, planetMaterial);
+    this.mesh.position.set(this.distance, 0, 0)
+  }
+
+  revolve() {
+    this.angle += this.revolutionSpeed;
+    this.reposition();
+  }
+
+  reposition() {
+    this.mesh.position.set(
+      this.distance * Math.cos(this.angle),
+      this.distance * Math.sin(this.angle),
+      0
+    );
+  }
+}
