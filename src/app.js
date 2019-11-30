5b91906457f08ff50156d9de6bbe481019b1764f
Jeremy Marin <marin.jeremy@gmail.com>
2019-12-01 00:02:59 +0900

    Make Scene component

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import * as THREE from 'three';
 
-import { init } from './scene.js'
+import Scene from './scene.js'
 // import "./App.css";
 
 class App extends Component {
-  componentDidMount() {
-    init(this.mount);
-  }
   render() {
     return (
-      <div ref={ref => (this.mount = ref)} />
+      <Scene />
     );
   }
 }
--- src/scene.js
+++ src/scene.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from './js/lib/OrbitControls.js';
 
@@ -13,66 +14,72 @@ import GalaxyTexturePZ from './textures/stars-1/pz.png';
 
 const PLANETS_COUNT = 10
 
-const init = (ref) => {
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  ref.appendChild(renderer.domElement);
-  
-  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100000);
-  camera.position.set(0, 0, 2000);
-  
-  let controls = new OrbitControls( camera, renderer.domElement );
-  controls.minDistance = 500;
-  controls.maxDistance = 8000;
-  
-  const scene = new THREE.Scene();
-  const textureLoader = new THREE.TextureLoader();
-  
-  const sunGeometry = new THREE.SphereGeometry(150, 20, 20);
-  const sunTexture = textureLoader.load(SunTexture);
-  const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
-  const sun = new THREE.Mesh(sunGeometry, sunMaterial);
-  sun.rotation.x = 90;
-  scene.add(sun);
-  
-  const sunlight = new THREE.PointLight(0xffffff, 2, 10000);
-  sunlight.position.set(0, 0, 0);
-  scene.add(sunlight);
-  
-  const skyboxTexture = new THREE.CubeTextureLoader().load([
-    GalaxyTexturePX,
-    GalaxyTextureNX,
-    GalaxyTexturePY,
-    GalaxyTextureNY,
-    GalaxyTexturePZ,
-    GalaxyTextureNZ,
-  ]);
-  scene.background = skyboxTexture;
-  
-  let planets = [];
-  for (let i = 0; i < PLANETS_COUNT; i++) {
-    const planet = new Planet(planets);
-    scene.add(planet.mesh);
-    planets.push(planet);
+export default class Scene extends React.Component {
+  componentDidMount() {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    this.mount.appendChild(renderer.domElement);
+    
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100000);
+    camera.position.set(0, 0, 2000);
+    
+    let controls = new OrbitControls( camera, renderer.domElement );
+    controls.minDistance = 500;
+    controls.maxDistance = 8000;
+    
+    const scene = new THREE.Scene();
+    const textureLoader = new THREE.TextureLoader();
+    
+    const sunGeometry = new THREE.SphereGeometry(150, 20, 20);
+    const sunTexture = textureLoader.load(SunTexture);
+    const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
+    const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+    sun.rotation.x = 90;
+    scene.add(sun);
+    
+    const sunlight = new THREE.PointLight(0xffffff, 2, 10000);
+    sunlight.position.set(0, 0, 0);
+    scene.add(sunlight);
+    
+    const skyboxTexture = new THREE.CubeTextureLoader().load([
+      GalaxyTexturePX,
+      GalaxyTextureNX,
+      GalaxyTexturePY,
+      GalaxyTextureNY,
+      GalaxyTexturePZ,
+      GalaxyTextureNZ,
+    ]);
+    scene.background = skyboxTexture;
+    
+    let planets = [];
+    for (let i = 0; i < PLANETS_COUNT; i++) {
+      const planet = new Planet(planets);
+      scene.add(planet.mesh);
+      planets.push(planet);
+    }
+    
+    const update = () => {
+      planets.forEach(planet => {
+        planet.revolve();
+      })
+    };
+    
+    const render = () => {
+      renderer.render(scene, camera);
+    };
+    
+    const animationLoop = () => {
+      requestAnimationFrame( animationLoop );
+      update();
+      render();
+    }
+    
+    animationLoop();
   }
-  
-  const update = () => {
-    planets.forEach(planet => {
-      planet.revolve();
-    })
-  };
-  
-  const render = () => {
-    renderer.render(scene, camera);
-  };
-  
-  const gameloop = () => {
-    requestAnimationFrame( gameloop );
-    update();
-    render();
-  }
-  
-  gameloop();
-}
 
-export { init };
+  render() {
+    return (
+      <div id='scene' ref={ref => (this.mount = ref)} />
+    );
+  }
+}
